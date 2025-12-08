b32da59363b57eee0152c5500d8ce59b7a9e4f5b
ramyvictor <ramy.victor@gmail.com>
2025-12-08 20:21:19 +0100

    Update base URL in docusaurus.config.ts and restore CNAME file for custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://xrm.world',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Xrm-World-Docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
